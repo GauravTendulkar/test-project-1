dec3d1458fd891d56f22dbb317d810c967536d5c
Gaurav Tendulkar <gaurav.tendulkar26@gmail.com>
2025-05-02 10:44:21 +0530

    cherry picked div() commit

--- index.js
+++ index.js
@@ -2,4 +2,16 @@ const sub = (a, b) =>{
     return a - b
 }
 
-console.log(sub(2,1))
+console.log(sub(2, 1))
+
+const div = (a, b) => {
+    return a / b
+}
+
+console.log(div(2, 3))
+
+const mul = (a, b) => {
+    return a * b
+}
+
+console.log(mul(2, 3))
